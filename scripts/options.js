485abe06d08703aab7e7e481f16ba8814e603168
cheap-glitch <cheap.glitch@gmail.com>
2020-02-20 12:58:55 +0100

    Supplement options page layout

--- scripts/options.js
+++ scripts/options.js
@@ -8,10 +8,10 @@
 (async function()
 {
 	// Autofill the inputs with the saved preferences
-	document.getElementById('input-github-token').value = (await browser.storage.sync.get({ githubPersonalToken: ''})).githubPersonalToken;
+	document.getElementById('githubPersonalToken').value = (await browser.storage.sync.get({ githubPersonalToken: ''})).githubPersonalToken;
 
 	// Save the GitHub token in the sync storage
-	document.getElementById('input-github-token').addEventListener('input', async function(e)
+	document.getElementById('githubPersonalToken').addEventListener('input', async function(e)
 	{
 		try {
 			await browser.storage.sync.set({ githubPersonalToken: e.target.value.trim() });
