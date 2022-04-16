daa883b8f310ab3445fb8d24492dc601f32ad31d
cheap-glitch <cheap.glitch@gmail.com>
2022-04-16 11:12:58 +0200

    Meta: Drop messages-related helpers and simplify notifications helper

--- src/lib/helpers/browser.ts
+++ src/lib/helpers/browser.ts
@@ -1,30 +1,8 @@
 export const { version } = browser.runtime.getManifest();
 
-type MessageId = 'updateTargets';
-
-type NotificationId = 'targetsUpdate' | 'error';
-
-export async function notify(id: NotificationId, title: string, message: string): Promise<void> {
-	if (await browser.notifications.update(id, { type: 'basic', title, message })) {
-		// The notification already exists and was succesfully updated
-		return;
-	}
-
-	await browser.notifications.create(id, { type: 'basic', title, message });
-}
-
-export function sendMessage(message: MessageId): Promise<void> {
-	return browser.runtime.sendMessage(message);
-}
-
-export function setupMessageListeners(callbacks: Record<MessageId, () => Promise<void>>): void {
-	browser.runtime.onMessage.addListener((message: MessageId) => {
-		for (const [callbackMessage, callback] of Object.entries(callbacks)) {
-			if (message === callbackMessage) {
-				void callback();
-			}
-		}
-	});
+export function notify(title: string, message: string): Promise<string> {
+	// Firefox doesn't support notification.update yet https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/notifications/update#browser_compatibility
+	return browser.notifications.create('warpgate-notification', { type: 'basic', title, message });
 }
 
 export function openUrl(url: string, disposition: browser.omnibox.OnInputEnteredDisposition): void {
